f68533918434dddd464f6319e0ae9559742d2ff8
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-10-19 22:17:25 +0300

    feat: implement jwtAuthGuard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { SequelizeModule } from '@nestjs/sequelize';
@@ -9,6 +9,7 @@ import { Director } from 'src/director/director.model';
 import { DirectorUser } from 'src/director_user/director_user';
 import { ActorUser } from 'src/actor_user/actor_user';
 import { MovieUser } from 'src/movie_user/movie_user';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UserController],
@@ -23,6 +24,7 @@ import { MovieUser } from 'src/movie_user/movie_user';
       ActorUser,
       MovieUser,
     ]),
+    forwardRef(() => AuthModule),
   ],
   exports: [UserService],
 })
